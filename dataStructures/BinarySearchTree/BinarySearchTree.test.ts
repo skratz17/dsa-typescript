599b21c63157584f0fa3262795e526a77b89dac5
Jacob Eckert <jweckert17@gmail.com>
2021-01-28 14:15:41 -0600

    implement delete node by value

--- dataStructures/BinarySearchTree/BinarySearchTree.test.ts
+++ dataStructures/BinarySearchTree/BinarySearchTree.test.ts
@@ -58,4 +58,110 @@ describe('unbalanced binary search tree functionality', () => {
     bst.insert(3);
     expect(bst.search(10)).toEqual({ val: 10, left: null, right: null });
   });
+
+  test('delete leaf node', () => {
+    const bst = new BinarySearchTree<number>((a, b) => a - b);
+
+    bst.insert(5);
+    bst.insert(10);
+    bst.insert(3);
+
+    bst.delete(3);
+    expect(bst.root.val).toEqual(5);
+    expect(bst.root.right.val).toEqual(10);
+    expect(bst.root.left).toBeNull();
+  });
+
+  test('delete node with right child', () => {
+    const bst = new BinarySearchTree<number>((a, b) => a - b);
+
+    bst.insert(5);
+    bst.insert(10);
+    bst.insert(3);
+    bst.insert(4);
+
+    bst.delete(3);
+    expect(bst.root.val).toEqual(5);
+    expect(bst.root.right.val).toEqual(10);
+    expect(bst.root.left.val).toEqual(4);
+  });
+
+  test('delete node with left child', () => {
+    const bst = new BinarySearchTree<number>((a, b) => a - b);
+
+    bst.insert(5);
+    bst.insert(10);
+    bst.insert(3);
+    bst.insert(2);
+
+    bst.delete(3);
+    expect(bst.root.val).toEqual(5);
+    expect(bst.root.right.val).toEqual(10);
+    expect(bst.root.left.val).toEqual(2);
+  });
+
+  test('delete node with two children where successor is right child', () => {
+    const bst = new BinarySearchTree<number>((a, b) => a - b);
+
+    bst.insert(5);
+    bst.insert(10);
+    bst.insert(3);
+
+    bst.delete(5);
+    expect(bst.root.val).toEqual(10);
+    expect(bst.root.left.val).toEqual(3);
+    expect(bst.root.right).toBeNull();
+  });
+
+  test('delete node with two children where successor is in right child\'s left subtree', () => {
+    const bst = new BinarySearchTree<number>((a, b) => a - b);
+
+    bst.insert(5);
+    bst.insert(10);
+    bst.insert(7);
+    bst.insert(3);
+    bst.insert(13);
+    bst.insert(11);
+    bst.insert(12);
+
+    /* verify structure is
+
+            5
+          /   \
+        3       10
+               /  \
+              7     13
+                   /
+                  11
+                    \
+                     12
+    */
+
+    expect(bst.root.val).toEqual(5);
+    expect(bst.root.left.val).toEqual(3);
+    expect(bst.root.right.val).toEqual(10);
+    expect(bst.root.right.left.val).toEqual(7);
+    expect(bst.root.right.right.val).toEqual(13);
+    expect(bst.root.right.right.left.val).toEqual(11);
+    expect(bst.root.right.right.left.right.val).toEqual(12);
+
+    /* remove node with val = 10 and verify structure becomes:
+            5
+          /   \
+        3       11
+               /  \
+              7     13
+                   /
+                  12
+    */
+
+    bst.delete(10);
+
+    expect(bst.root.val).toEqual(5);
+    expect(bst.root.left.val).toEqual(3);
+    expect(bst.root.right.val).toEqual(11);
+    expect(bst.root.right.left.val).toEqual(7);
+    expect(bst.root.right.right.val).toEqual(13);
+    expect(bst.root.right.right.left.val).toEqual(12);
+  });
 });
--- dataStructures/BinarySearchTree/BinarySearchTree.ts
+++ dataStructures/BinarySearchTree/BinarySearchTree.ts
@@ -39,4 +39,35 @@ export class BinarySearchTree<T> {
     else if(this.compare(node.val, val) > 0) return this._search(node.left, val);
     else return this._search(node.right, val);
   }
+
+  delete(val: T) {
+    this.root = this._delete(this.root, val);
+  }
+
+  _delete(node: BinarySearchTreeNode<T>, val: T) {
+    if(!node) return null;
+
+    if(node.val === val) {
+      // delete leaf node
+      if(node.left === null && node.right === null) return null;
+
+      // delete with one child
+      else if((node.left === null && node.right) || (node.right === null && node.left)) {
+        return node.left ? node.left : node.right;
+      }
+
+      // delete with two children - successor is leftmost child in right subtree
+      else {
+        let successor = node.right;
+        while(successor.left) successor = successor.left;
+        node.val = successor.val;
+        node.right = this._delete(node.right, successor.val);
+      }
+    }
+
+    else if(this.compare(node.val, val) > 0) node.left = this._delete(node.left, val);
+    else node.right = this._delete(node.right, val);
+
+    return node;
+  }
 }
