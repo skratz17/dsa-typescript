1df8137b6ded148763541526b116e20a46f6c562
Jacob Eckert <jweckert17@gmail.com>
2021-01-28 20:50:19 -0600

    implement quick sort

--- algorithms/sort/bubbleSort.ts
+++ algorithms/sort/bubbleSort.ts
@@ -1,4 +1,6 @@
-export const bubbleSort = <T>(arr: T[], compare: (a: T, b: T) => number) => {
+import { CompareFunc } from './types';
+
+export const bubbleSort = <T>(arr: T[], compare: CompareFunc<T>) => {
   for(let i = 1; i < arr.length; i++) {
     for(let j = 0; j < arr.length - i; j++) {
       if(compare(arr[j], arr[j + 1]) > 0) {
